43b797b0ed0d22852056d13734bd95cf31c57e6b
Yuyang ZHOU <zhouyy12358@gmail.com>
2018-04-14 22:06:00 +0800

    Java HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//package trial;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("HelloWorld!");
+	}
+	
+	public static void packageTest(){
+		System.out.println("Success!");
+	}
+}
